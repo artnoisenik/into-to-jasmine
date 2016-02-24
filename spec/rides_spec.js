8e6a8573b34359eb1d97e202ec5a158f8aeec481
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-24 13:16:56 -0700

    adding to spec

---
+++ spec/rides_spec.js
@@ -0,0 +1,28 @@
+var rides = require('../lib/rides.js');
+
+describe('Rides', function(){
+  it('is child tall enough', function(){
+    var child1 = {
+      height: 3,
+      name: "Julian",
+      age: 3
+    };
+
+    var child2 = {
+      height: 4,
+      name: "Cero",
+      age: 4
+    };
+
+    var ride1 = {
+      minHeight: 3,
+      maxHeight: 7
+    };
+
+    var ride2 = {
+      minHeight: 4,
+      maxHeight: 7
+    };
+
+  });
+})
